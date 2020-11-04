98ece8d40c2b6b3b057187e6d7f6bdbdb5542066
EC2 Default User <ec2-user@ip-172-31-39-61.us-west-2.compute.internal>
2020-11-04 02:40:40 +0000

    web not working

--- src/tritonhttp/http_request_handler.go
+++ src/tritonhttp/http_request_handler.go
@@ -4,6 +4,7 @@ import (
 	"net"
 	"os"
 	"strconv"
+	"time"
 
 	// "fmt"
 	"log"
@@ -31,38 +32,50 @@ func (hs *HttpServer) handleConnection(conn net.Conn) {
 	// Update any ongoing requests
 	// If reusing read buffer, truncate it before next read
 
-	delimiter := "\n"
+	delimiter := "\r\n"
 	remaining := ""
 	var res HttpResponseHeader
-	// timeoutDuration := 5 * time.Second
+	timeoutDuration := 5 * time.Second
 	// bufReader := bufio.NewReader(conn)
 	w := bufio.NewWriter(conn)
 	response := ""
+	buf := make([]byte, 10)
 	for {
 
-		// conn.SetReadDeadline(time.Now().Add(timeoutDuration))
+		conn.SetReadDeadline(time.Now().Add(timeoutDuration))
 		// defer conn.Close()
-		buf := make([]byte, 10)
+
 		// size, err := bufReader.Read(buf)
 		size, err := conn.Read(buf)
 		if err != nil {
-			// log.Println(err)
+			log.Println(err)
 			// break
 		}
 		data := buf[:size]
 		remaining = remaining + string(data)
 		// bufReader.Reset(bufReader)
 		// log.Println("original msg: " + remaining)
-		for strings.Contains(remaining, delimiter) {
+		for strings.Contains(remaining, "\r\n\r\n") {
 			// conn.SetReadDeadline(time.Now().Add(timeoutDuration))
 			// log.Println("delimiter: " + delimiter)
 			// log.Println("original msg: " + remaining)
-			idx := strings.Index(remaining, delimiter)
-			msg := remaining[:idx]
+			log.Println("original:" + remaining)
+			idx := strings.Index(remaining, "\r\n\r\n")
+			msg := remaining[:idx] // whole requests
+			remaining = remaining[idx+1:]
+			reqSlice := strings.Split(msg, delimiter) // request
+			log.Println(len(reqSlice))
+			if len(reqSlice) < 2 {
+				w.WriteString("400 Bad Request2")
+				w.Flush()
+				break
+			}
+			log.Println("message:" + msg)
+			log.Println("left:" + remaining)
+			initialLine := reqSlice[0] // get
 			// log.Println("request: " + msg)
 			// initial := remaining[idx+1:]
-			initialLine := strings.Fields(msg)
-			remaining = remaining[idx+1:]
+
 			// log.Println("left msg: " + remaining)
 			// log.Println(initialLine[0])
 			// w.WriteString(initialLine[0])
@@ -71,37 +84,39 @@ func (hs *HttpServer) handleConnection(conn net.Conn) {
 			// log.Println(strings.HasPrefix(msg, "Host"))
 			// log.Println("prefix conn: ")
 			// log.Println(strings.HasPrefix(msg, "Connection:"))
-			if strings.HasPrefix(msg, "GET") {
-				// log.Println(initialLine)
-				checkHTTP := strings.TrimSpace(initialLine[len(initialLine)-1]) == "HTTP/1.1"
-				checkLength := len(initialLine) == 3
-				checkGet := initialLine[0] == "GET"
-				if !checkLength {
+			// initialLine = GET /index.html HTTP/1.1
+			if strings.HasPrefix(initialLine, "GET") {
+				firstR := strings.Split(initialLine, " ")
+				log.Println(firstR)
+				// checkHTTP := firstR[0] == "HTTP/1.1"
+				// checkLength := len(initialLine) == 3
+				// checkGet := firstR[0] == "GET"
+				if len(firstR) != 3 {
 					// req.validInitial = false
 					// w.WriteString("400 Bad Request1")
 					// w.Flush()
 					log.Println("error1")
-					log.Println(len(initialLine))
+					log.Println(firstR)
+					log.Println(len(firstR))
 					w.WriteString("400 Bad Request2")
 					w.Flush()
 					// hs.handleBadRequest(conn)
 					break
-				} else if !checkHTTP {
+				} else if firstR[2] != "HTTP/1.1" {
 					log.Println("error2")
-					log.Println(!checkHTTP)
-					log.Println(initialLine[len(initialLine)-1])
+					log.Println(firstR[2])
 					w.WriteString("400 Bad Request2")
 					w.Flush()
 					// hs.handleBadRequest(conn)
 					break
-				} else if !checkGet {
+				} else if firstR[0] != "GET" {
 					log.Println("error3")
 					log.Println(initialLine[0])
 					w.WriteString("400 Bad Request2")
 					w.Flush()
 					// hs.handleBadRequest(conn)
 					break
-				} else if !strings.HasPrefix(initialLine[1], "/") {
+				} else if !strings.HasPrefix(firstR[1], "/") {
 					w.WriteString("400 Bad Request2")
 					w.Flush()
 					log.Println("error4")
@@ -114,13 +129,13 @@ func (hs *HttpServer) handleConnection(conn net.Conn) {
 					// w.WriteString("Server: Go-Triton-Server-1.0\r\n")
 					// w.Flush()
 
-					url := hs.DocRoot + initialLine[1]
-					if initialLine[1] == "/" {
+					url := hs.DocRoot + firstR[1]
+					if firstR[1] == "/" {
 						log.Println("url is 11/")
-						url = hs.DocRoot + initialLine[1] + "index.html"
+						url = hs.DocRoot + firstR[1] + "index.html"
 					} else {
 						log.Println("url is " + url)
-						url = hs.DocRoot + initialLine[1]
+						url = hs.DocRoot + firstR[1]
 					}
 					log.Println(url)
 
@@ -163,7 +178,15 @@ func (hs *HttpServer) handleConnection(conn net.Conn) {
 					// w.Flush()
 
 				}
-			} else if strings.HasPrefix(msg, "Host") {
+			} else {
+				w.WriteString("400 Bad Request2")
+				w.Flush()
+				log.Println("error5")
+				break
+			}
+			secondLine := reqSlice[1]
+			log.Println("second l: " + secondLine)
+			if strings.HasPrefix(secondLine, "Host") {
 				// idxH := strings.Index(msg, ":")
 				// msgH := msg[idxH+1:]
 
@@ -171,21 +194,36 @@ func (hs *HttpServer) handleConnection(conn net.Conn) {
 				w.WriteString(response)
 				w.Flush()
 				// hs.sendResponse()
-			} else if strings.HasPrefix(msg, "Connection:") {
-				idxH := strings.Index(msg, ":")
-				msgH := msg[idxH+1:]
-				connection := strings.TrimSpace(msgH)
-				if connection == "close" {
-					res.connection = "close"
-					w.WriteString("Connection: closed\r\n")
-					w.Flush()
-					// conn.Close()
-					log.Println("Connection closed by request.")
-					return
-				} else {
-					res.connection = "no"
+			} else {
+				w.WriteString("400 Bad Request2")
+				w.Flush()
+				log.Println("error6")
+				break
+			}
+
+			if len(reqSlice) > 2 {
+				log.Println("handel connection")
+				if strings.HasPrefix(reqSlice[2], "Connection:") {
+					log.Println("handel connection2")
+					idxH := strings.Index(reqSlice[2], ":")
+					msgH := reqSlice[2][idxH+1:]
+					connection := strings.TrimSpace(msgH)
+					log.Println("conn msg:" + connection)
+					if connection == "close" {
+						res.connection = "close"
+						w.WriteString("Connection: closed\r\n")
+						w.Flush()
+						conn.Close()
+						log.Println("Connection closed by request.")
+						return
+					} else {
+						log.Println("not close")
+						res.connection = "no"
+					}
 				}
 			}
+
 		}
+		log.Println("finish requests")
 	}
 }
